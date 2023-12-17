8d16074e18d9a1e8b3474e43caa6dbc024066a5b
David Partyka <phdave2005@gmail.com>
2023-12-17 00:03:48 -0500

    feat: translated help text to non-english languages | COD-6.3

--- src/help/translation-map.js
+++ src/help/translation-map.js
@@ -27,79 +27,79 @@ export const TEXT_MAP = {
 	},
 	es: {
 		heading: 'Temas de Ayuda',
-		subheading: 'Usage Tips',
+		subheading: 'Consejos de Uso',
 		content: [
 			{
 				id: 0,
-				text: 'Select at least ONE search parameter from Formula, Lattice, Journal, or Additional.'
+				text: 'Seleccione al menos UN parámetro de búsqueda de Fórmula, Entramado, Diario o Adicional.'
 			},
 			{
 				id: 1,
-				text: 'Filters can be optionally selected.'
+				text: 'Los filtros se pueden seleccionar opcionalmente.'
 			},
 			{
 				id: 2,
-				text: 'Simple filters will not add more than a fraction of a second of additional scan time, but Advanced filters will require several additional seconds per filter.'
+				text: 'Los filtros simples no agregarán más de una fracción de segundo de tiempo de escaneo adicional, pero los filtros avanzados requerirán varios segundos adicionales por filtro.'
 			},
 			{
 				id: 3,
-				text: 'Search hits will be automatically downloaded as a .json file.'
+				text: 'Los resultados de búsqueda se descargarán automáticamente como un archivo .json.'
 			},
 			{
 				id: 4,
-				text: 'Language used can be changed under Settings. (as of December 2023, English, Spanish, French, and Italian are available)'
+				text: 'El idioma utilizado se puede cambiar en Configuración. (a partir de diciembre de 2023, están disponibles inglés, español, francés e italiano)'
 			}
 		]
 	},
 	fr: {
 		heading: 'Sujets d\'Aide',
-		subheading: 'Usage Tips',
+		subheading: 'Conseils d\'Utilisation',
 		content: [
 			{
 				id: 0,
-				text: 'Select at least ONE search parameter from Formula, Lattice, Journal, or Additional.'
+				text: 'Sélectionnez au moins UN paramètre de recherche parmi Formule, Treillis, Journal ou Supplémentaire.'
 			},
 			{
 				id: 1,
-				text: 'Filters can be optionally selected.'
+				text: 'Les filtres peuvent être sélectionnés en option.'
 			},
 			{
 				id: 2,
-				text: 'Simple filters will not add more than a fraction of a second of additional scan time, but Advanced filters will require several additional seconds per filter.'
+				text: 'Les filtres simples n\'ajouteront pas plus d\'une fraction de seconde de temps d\'analyse supplémentaire, mais les filtres avancés nécessiteront plusieurs secondes supplémentaires par filtre.'
 			},
 			{
 				id: 3,
-				text: 'Search hits will be automatically downloaded as a .json file.'
+				text: 'Les résultats de recherche seront automatiquement téléchargés sous forme de fichier .json.'
 			},
 			{
 				id: 4,
-				text: 'Language used can be changed under Settings. (as of December 2023, English, Spanish, French, and Italian are available)'
+				text: 'La langue utilisée peut être modifiée sous Paramètres. (à partir de décembre 2023, l\'anglais, l\'espagnol, le français et l\'italien sont disponibles)'
 			}
 		]
 	},
 	it: {
 		heading: 'Argomenti di Aiuto',
-		subheading: 'Usage Tips',
+		subheading: 'Consigli per l\'uso',
 		content: [
 			{
 				id: 0,
-				text: 'Select at least ONE search parameter from Formula, Lattice, Journal, or Additional.'
+				text: 'Selezionare almeno UN parametro di ricerca tra Formula, Lattice, Diario o Aggiuntivo.'
 			},
 			{
 				id: 1,
-				text: 'Filters can be optionally selected.'
+				text: 'I filtri possono essere selezionati facoltativamente.'
 			},
 			{
 				id: 2,
-				text: 'Simple filters will not add more than a fraction of a second of additional scan time, but Advanced filters will require several additional seconds per filter.'
+				text: 'I filtri semplici non aggiungeranno più di una frazione di secondo di tempo di scansione aggiuntivo, ma i filtri avanzati richiederanno diversi secondi aggiuntivi per filtro.'
 			},
 			{
 				id: 3,
-				text: 'Search hits will be automatically downloaded as a .json file.'
+				text: 'I risultati della ricerca verranno scaricati automaticamente come file .json.'
 			},
 			{
 				id: 4,
-				text: 'Language used can be changed under Settings. (as of December 2023, English, Spanish, French, and Italian are available)'
+				text: 'La lingua utilizzata può essere modificata in Impostazioni. (da dicembre 2023 sono disponibili inglese, spagnolo, francese e italiano)'
 			}
 		]
 	}
